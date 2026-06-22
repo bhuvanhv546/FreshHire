a6577c16495dc8281e0f81687e1d93fb55763e2f
Bhuvan H V <bhuvanhv570@gmail.com>
2026-06-22 17:29:01 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,23 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true
   }
 });
-
 app.use(helmet());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fresh-hire-theta.vercel.app",
+      /\.vercel\.app$/
+    ],
+    credentials: true
+  })
+);
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
