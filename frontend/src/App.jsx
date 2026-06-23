98223eb4b2a0b4ed8315bb9949b9d75780376f58
Bhuvan H V <bhuvanhv570@gmail.com>
2026-06-23 11:56:34 +0530

    test home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Layout/Navbar";
 
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<h1>Home Working</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
