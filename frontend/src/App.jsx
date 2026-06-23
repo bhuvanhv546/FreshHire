565d92a086aea62b9a34b34c097ff3045e8c2564
Bhuvan H V <bhuvanhv570@gmail.com>
2026-06-23 11:58:42 +0530

    add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Layout/Navbar";
 
 import Home from "./pages/Home";
+import Login from "./pages/Auth/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
