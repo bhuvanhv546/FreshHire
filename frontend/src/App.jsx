191acfebd5edc355c5cb2891690015ff9bffa453
Bhuvan H V <bhuvanhv570@gmail.com>
2026-06-23 12:02:34 +0530

    test register route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Layout/Navbar";
 
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
